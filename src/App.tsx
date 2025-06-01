d6c5cbf6f85fa2ae5ff40439db92bd715104b61b
Samuel Kofi-Opata <samkofiopata@gmail.com>
2025-06-01 18:09:13 -0400

    price changes

--- src/App.tsx
+++ src/App.tsx
@@ -65,14 +65,14 @@ function StorePage({
   const [hoveredProductId, setHoveredProductId] = useState<number | null>(null);
 
   const dummyProducts = [
-    { id: 1, name: 'TIME F*CKS TEE', price: '$29.99', image: '/images/TIME F* FRONT.png', hoverImage: '/images/TIME F* BACK.png' },
-    { id: 2, name: 'PSP TEE', price: '$29.99', image: '/images/PSP TEE.png', hoverImage: '/images/imageloading.png' },
-    { id: 3, name: 'NASA HOODIE', price: '$99.99', image: '/images/NASA HOODIE FRONT.png', hoverImage: '/images/NASA HOODIE BACK.png' },
-    { id: 4, name: 'MEMBER HOODIE', price: '$39.99', image: '/images/2035 HOODIE FRONT.png', hoverImage: '/images/2035 HOODIE BACK.png' },
-    { id: 5, name: 'O DUTCH JERSEY', price: '$29.99', image: '/images/O DUTCH JERSEY.png', hoverImage: '/images/imageloading.png' },
-    { id: 6, name: 'TORTURE BEANIE', price: '$29.99', image: '/images/TORTURE_BEANIE_FRONT.png', hoverImage: '/images/TORTURE_BEANIE_BACK.png' },
-    { id: 7, name: 'WWW BEANIE', price: '$99.99', image: '/images/WWW.OMNI.COM BEANIE OUTER.png', hoverImage: '/images/WWW.OMNI.COM INSIDE.png' },
-    { id: 8, name: 'DENIM SPACE TROUSERS', price: '$39.99', image: '/images/O SPACE TROUSERS.png', hoverImage: '/images/imageloading.png' },
+    { id: 1, name: 'TIME F*CKS TEE', price: '$30', image: '/images/TIME F* FRONT.png', hoverImage: '/images/TIME F* BACK.png' },
+    { id: 2, name: 'PSP TEE', price: '$30', image: '/images/PSP TEE.png', hoverImage: '/images/imageloading.png' },
+    { id: 3, name: 'NASA HOODIE', price: '$80', image: '/images/NASA HOODIE FRONT.png', hoverImage: '/images/NASA HOODIE BACK.png' },
+    { id: 4, name: 'MEMBER HOODIE', price: '$50', image: '/images/2035 HOODIE FRONT.png', hoverImage: '/images/2035 HOODIE BACK.png' },
+    { id: 5, name: 'O DUTCH JERSEY', price: '$65', image: '/images/O DUTCH JERSEY.png', hoverImage: '/images/imageloading.png' },
+    { id: 6, name: 'TORTURE BEANIE', price: '$17', image: '/images/TORTURE_BEANIE_FRONT.png', hoverImage: '/images/TORTURE_BEANIE_BACK.png' },
+    { id: 7, name: 'WWW BEANIE', price: '$20', image: '/images/WWW.OMNI.COM BEANIE OUTER.png', hoverImage: '/images/WWW.OMNI.COM INSIDE.png' },
+    { id: 8, name: 'DENIM SPACE TROUSERS', price: '$125', image: '/images/O SPACE TROUSERS.png', hoverImage: '/images/imageloading.png' },
   ];
 
   const addToCart = (product: { id: number; name: string; price: string; image: string }) => {
